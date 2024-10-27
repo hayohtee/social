cf85cd4cf59684c7e518e91365f4863a93d394c9
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-27 08:16:45 +0100

    Add serve method

---
+++ cmd/api/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func (app *application) serve(mux http.Handler) error {
+	srv := http.Server {
+		Addr: app.config.addr,
+		Handler: mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		IdleTimeout: time.Minute,
+	}
+
+	log.Printf("starting server at %s\n", srv.Addr)
+
+	return srv.ListenAndServe()
+}
