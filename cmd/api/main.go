0ee3c6f3db0061db4ec3dd85c9ff8a5fa5fbbdde
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-27 08:17:34 +0100

    Update main.go to spin up http server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main()  {
-	
+import "log"
+
+func main() {
+	cfg := config{
+		addr: ":8080",
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	routes := app.routes()
+
+	if err := app.serve(routes); err != nil {
+		log.Fatal(err)
+	}
 }
