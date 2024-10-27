3b9fd17fb6317a993d87372ba163e5585074e3b8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-27 11:54:12 +0100

    Add database package

---
+++ internal/database/database.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+func New(addr string, maxOpenConns, maxIdleConns int, maxIdleTime string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
