26f3310e76f2250baefc021534cda54714fd2fea
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-27 12:14:09 +0100

    Change fallback value for maxIdleTime to 15mins

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,7 +21,7 @@ func main() {
 			addr:         env.GetString("DB_ADDR", "postgres://user:password@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15mins"),
 		},
 	}
 
