fd348e9a325fc6b96a816e95d08461ce8ade3e8d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-27 11:33:57 +0100

    Add code for creating new post in the database

--- internal/repository/posts.go
+++ internal/repository/posts.go
@@ -3,12 +3,26 @@ package repository
 import (
 	"context"
 	"database/sql"
+
+	"github.com/hayohtee/social/internal/data"
 )
 
 type PostsRepository struct {
 	db *sql.DB
 }
 
-func (p *PostsRepository) Create(ctx context.Context) error {
-	return nil
+func (p *PostsRepository) Create(ctx context.Context, post *data.Post) error {
+	query := `
+		INSERT INTO posts (content, title, user_id, tags)
+		VALUES($1, $2, $3, $4)
+		RETURNING id, created_at, updated_at
+	`
+
+	args := []any{post.Content, post.Title, post.UserID, post.Tags}
+
+	return p.db.QueryRowContext(ctx, query, args...).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
 }
