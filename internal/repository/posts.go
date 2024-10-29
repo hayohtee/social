d6764682a4a2808235452af3fbdab0e3a91d4120
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-29 13:03:08 +0100

    Add GetPostByID method

--- internal/repository/posts.go
+++ internal/repository/posts.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"database/sql"
+	"errors"
 
 	"github.com/hayohtee/social/internal/data"
 )
@@ -25,4 +26,33 @@ func (p *PostsRepository) Create(ctx context.Context, post *data.Post) error {
 		&post.CreatedAt,
 		&post.UpdatedAt,
 	)
+}
+
+func (p *PostsRepository) GetByID(ctx context.Context, id int64) (data.Post, error) {
+	query := `
+		SELECT id, user_id, title, content, tags, created_at, updated_at 
+		FROM posts
+		WHERE id = $1`
+
+	var post data.Post
+	err := p.db.QueryRowContext(ctx, query, id).Scan(
+		&post.ID,
+		&post.UserID,
+		&post.Title,
+		&post.Content,
+		&post.Tags,
+		&post.CreatedAt,
+		&post.UpdatedAt,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return data.Post{}, ErrNotFound
+		default:
+			return data.Post{}, err
+		}
+	}
+
+	return post, nil
 }
