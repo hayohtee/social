c8b223907387b3ca3c98ebbbffb5716c5a55def3
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-29 13:04:12 +0100

    Add routes for GET /posts/{postID}

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,10 @@ func (app *application) routes() http.Handler {
 
 		r.Route("/posts", func(r chi.Router) {
 			r.Post("/", app.createPostHandler)
+
+			r.Route("/{postID}", func(r chi.Router) {
+				r.Get("/", app.getPostHandler)
+			})
 		})
 	})
 
