b36e0c9aae9ae5952291ccec521b3db24d4608a6
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-30 12:02:30 +0100

    Add badRequestErrorResponse

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -9,3 +9,8 @@ func (app *application) internalServerErrorResponse(w http.ResponseWriter, r *ht
 	log.Printf("internal server error: %s path: %s error: %s\n", r.Method, r.URL.Path, err)
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem when processing the request")
 }
+
+func (app *application) badRequestErrorResponse(w http.ResponseWriter, r *http.Request, err error, message string) {
+	log.Printf("bad request error: %s path: %s error: %s\n", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusBadRequest, message)
+}
