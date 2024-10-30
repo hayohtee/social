e2c840240168ac15dadad71be95f1dba24597fdb
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-30 11:50:16 +0100

    Update the log format for internalServerErrorResponse

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -6,6 +6,6 @@ import (
 )
 
 func (app *application) internalServerErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
-	log.Printf("internal server error: %s path: %s error: %w\n", r.Method, r.URL.Path, err)
+	log.Printf("internal server error: %s path: %s error: %s\n", r.Method, r.URL.Path, err)
 	writeJSONError(w, http.StatusInternalServerError, "the server encountered a problem when processing the request")
 }
