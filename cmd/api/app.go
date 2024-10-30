2ef58a1fc9304beec37175e7293d25354c7cf845
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-30 12:17:26 +0100

    Add validate field to application struct

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/hayohtee/social/internal/repository"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/hayohtee/social/internal/repository"
+)
 
 type application struct {
 	config     config
 	repository repository.Repository
+	validate   *validator.Validate
 }
