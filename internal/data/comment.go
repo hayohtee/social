6e72bba71acfe13421ad6e9b5a478dfcb97b5176
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-31 12:21:19 +0100

    Add json tags

--- internal/data/comment.go
+++ internal/data/comment.go
@@ -3,18 +3,18 @@ package data
 import "time"
 
 type Comment struct {
-	ID        int64
-	PostID    int64
-	UserID    int64
-	Content   string
-	CreatedAt time.Time
+	ID        int64     `json:"id"`
+	PostID    int64     `json:"post_id"`
+	UserID    int64     `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type CommentWithUser struct {
-	ID        int64
-	PostID    int64
-	UserID    int64
-	Content   string
-	UserName  string
-	CreatedAt time.Time
+	ID        int64     `json:"id"`
+	PostID    int64     `json:"post_id"`
+	UserID    int64     `json:"user_id"`
+	Content   string    `json:"content"`
+	UserName  string    `json:"user_name"`
+	CreatedAt time.Time `json:"created_at"`
 }
