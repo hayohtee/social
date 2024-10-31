6e9ca5cb683231a2697bbd27fc42a422120963cc
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-31 12:14:04 +0100

    Add CommentsRepository with GetByPostID method

---
+++ internal/repository/comments.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/hayohtee/social/internal/data"
+)
+
+type CommentsRepository struct {
+	db *sql.DB
+}
+
+func (c *CommentsRepository) GetByPostID(ctx context.Context, postID int64) ([]data.CommentWithUser, error) {
+	query := `
+		SELECT c.id, c.post_id, c.user_id, c.content, c.created_at, users.username
+		FROM comments c
+		JOIN users u on user.id = c.user_id
+		WHERE c.post_id = $1
+		ORDER BY c.created_at DESC`
+
+	rows, err := c.db.QueryContext(ctx, query, postID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var comments []data.CommentWithUser
+	for rows.Next() {
+		var comment data.CommentWithUser
+		err := rows.Scan(
+			&comment.ID,
+			&comment.PostID,
+			&comment.UserID,
+			&comment.Content,
+			&comment.CreatedAt,
+			&comment.UserName,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		comments = append(comments, comment)
+	}
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+	return comments, nil
+}
