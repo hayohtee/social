5c9fc122b971d427718b7329bca8ea7b78c8d69a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-01 18:31:01 +0100

    Add ValidatePost method

--- internal/data/post.go
+++ internal/data/post.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/hayohtee/social/internal/validator"
+)
 
 type Post struct {
 	ID        int64
@@ -10,4 +14,14 @@ type Post struct {
 	Tags      []string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func ValidatePost(v *validator.Validator, post Post) {
+	v.Check(post.Title != "", "title", "must be provided")
+	v.Check(len(post.Title) <= 100, "title", "must not be more than 100 bytes long")
+
+	v.Check(post.Content != "", "content", "must be provided")
+	v.Check(len(post.Content) <= 1000, "content", "must be more than 1000 bytes long")
+
+	v.Check(post.UserID < 0, "user_id", "must be a positive number")
 }
