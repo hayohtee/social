f80a48f293f8b42b8f3b1a51067dce12ddbe98f4
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-01 17:55:35 +0100

    Add getPostFromContext helper method

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/hayohtee/social/internal/data"
 )
 
 func readIDParam(r *http.Request, key string) (int64, error) {
 	idParam := chi.URLParam(r, key)
 	return strconv.ParseInt(idParam, 10, 64)
+}
+
+func getPostFromContext(ctx context.Context) (data.Post, bool) {
+	post, ok := ctx.Value(postKey).(data.Post)
+	return post, ok
 }
