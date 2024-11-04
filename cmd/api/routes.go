e2b9127d56e6bf52cc9128610f3ddf6791cf6bba
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-04 12:30:11 +0100

    Use the usersContextMiddleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -37,8 +37,10 @@ func (app *application) routes() http.Handler {
 
 		r.Route("/users", func(r chi.Router) {
 			r.Route("/{userID}", func(r chi.Router) {
-				r.Get("/", app.getUserHandler)
+				r.Use(app.userContextMiddleware)
 
+				r.Get("/", app.getUserHandler)
+				
 				r.Put("/follow", app.followUserHandler)
 				r.Delete("/follow", app.unFollowUserHandler)
 			})
