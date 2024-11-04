6519c99fc188d3b995e3598853ee7dd18967470f
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-04 13:02:10 +0100

    Add Follow and UnFollow method

--- internal/repository/followers.go
+++ internal/repository/followers.go
@@ -9,10 +9,27 @@ type FollowersRepository struct {
 	db *sql.DB
 }
 
-func (f *FollowersRepository) UnFollow(ctx context.Context, userID int64, followerID int64) error {
-	return nil
+func (f *FollowersRepository) UnFollow(ctx context.Context, userID, followerID int64) error {
+	query := `
+		DELETE followers
+		WHERE user_id = $1 AND follower_id = $2`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	_, err := f.db.ExecContext(ctx, query, userID, followerID)
+
+	return err
 }
 
-func (f *FollowersRepository) Follow(ctx context.Context, userID int64, followerID int64) error {
-	return nil
+func (f *FollowersRepository) Follow(ctx context.Context, userID, followerID int64) error {
+	query := `
+		INSERT INTO followers (user_id, follower_id)
+		VALUES ($1, $2)`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	_, err := f.db.ExecContext(ctx, query, userID, followerID)
+	return err
 }
