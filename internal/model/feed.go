b190adcd7568722bef5aef3860a2c6f1b86d69e6
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-05 14:09:20 +0100

    Add Feed struct

--- internal/model/feed.go
+++ internal/model/feed.go
@@ -1,5 +1,15 @@
 package model
 
+import "time"
+
 type Feed struct {
-	
+	PostID        int64
+	Title         string
+	UserID        int64
+	Tags          []string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Content       string
+	Username      string
+	CommentsCount int
 }
