39057b621bf1d59a99c3933761ed731b3743f3d2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-06 12:15:50 +0100

    And new helper methods for reading values from query parameters

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/hayohtee/social/internal/model"
+	"github.com/hayohtee/social/internal/validator"
 )
 
 func readIDParam(r *http.Request, key string) (int64, error) {
@@ -22,4 +24,25 @@ func getPostFromContext(ctx context.Context) (model.Post, bool) {
 func getUserFromContext(ctx context.Context) (model.User, bool) {
 	user, ok := ctx.Value(userKey).(model.User)
 	return user, ok
+}
+
+func readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	return s
+}
+
+func (app *application) readInt(qs url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer")
+		return defaultValue
+	}
+	return i
 }
