9fa6461f3ee686e81944809e5a27c506e2cf4956
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-07 17:27:31 +0100

    Readd tags and search values from query parameters

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -16,6 +16,8 @@ func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Reques
 	filters.Page = readInt(qs, "page", 1, v)
 	filters.PageSize = readInt(qs, "page_size", 20, v)
 	filters.Sort = readString(qs, "sort", "ASC")
+	filters.Tags = readCSV(qs, "tags", []string{})
+	filters.Search = readString(qs, "search", "")
 
 	if data.ValidateFilters(v, filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
@@ -27,6 +29,10 @@ func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Reques
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
+	}
+
+	if feeds == nil {
+		feeds = []data.Feed{}
 	}
 
 	if err := app.writeJSON(w, http.StatusOK, envelope{"items": feeds}, nil); err != nil {
