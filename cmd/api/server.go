aaba1abd3ad0cff72f527ccbcf5f49724a86a0eb
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-10 10:14:44 +0100

    Remove godocs setup

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -4,21 +4,15 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/hayohtee/social/docs"
 )
 
 func (app *application) serve(mux http.Handler) error {
-	docs.SwaggerInfo.Version = version
-	docs.SwaggerInfo.BasePath = "/v1"
-	docs.SwaggerInfo.Host = app.config.addr
-
-	srv := http.Server {
-		Addr: app.config.addr,
-		Handler: mux,
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
 		WriteTimeout: 30 * time.Second,
-		ReadTimeout: 10 * time.Second,
-		IdleTimeout: time.Minute,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
 	}
 
 	log.Printf("starting server at %s\n", srv.Addr)
