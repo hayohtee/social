01ef95716af1dceeba9c43e24865b08739fcfb65
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-13 19:59:36 +0100

    Add generateToken helper function

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -54,4 +57,16 @@ func readCSV(qs url.Values, key string, defaultValue []string) []string {
 		return defaultValue
 	}
 	return strings.Split(csv, ",")
+}
+
+func generateToken() ([]byte, error) {
+	randomBytes := make([]byte, 16)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	encodedBytes := base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+	hash := sha256.Sum256([]byte(encodedBytes))
+	return hash[:], nil
 }
