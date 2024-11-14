5ed6ea64ef5de28b86887e3d8e384057a6e9b8fe
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-11-14 21:24:39 +0100

    Update UserRepository

--- internal/repository/users.go
+++ internal/repository/users.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"strings"
@@ -98,4 +99,77 @@ func (u *UsersRepository) createInvitation(ctx context.Context, tx *sql.Tx, toke
 		return err
 	}
 	return nil
+}
+
+func (u *UsersRepository) Activate(ctx context.Context, plainToken string) error {
+	return withTx(u.db, ctx, func(tx *sql.Tx) error {
+		user, err := getUserFromInvitation(ctx, tx, plainToken)
+		if err != nil {
+			return err
+		}
+		user.Activated = true
+		if err := u.update(ctx, user, tx); err != nil {
+			return err
+		}
+
+		if err := u.deleteUserInvitations(ctx, user.ID, tx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func getUserFromInvitation(ctx context.Context, tx *sql.Tx, plainToken string) (data.User, error) {
+	query := `
+		SELECT u.id, u.username, u.email, u.created_at, u.activated
+		FROM users u
+		JOIN user_invitations ui ON ui.user_id = u.id
+		WHERE ui.token = $1 AND ui.expiry > $2`
+
+	ctx, cancel := context.WithTimeout(ctx, queryTimeoutDuration)
+	defer cancel()
+
+	tokenHash := sha256.Sum256([]byte(plainToken))
+	var user data.User
+	err := tx.QueryRowContext(ctx, query, tokenHash[:], time.Now()).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.CreatedAt,
+		&user.Activated,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return data.User{}, ErrNotFound
+		default:
+			return data.User{}, err
+		}
+	}
+
+	return user, nil
+}
+
+func (u *UsersRepository) update(ctx context.Context, user data.User, tx *sql.Tx) error {
+	query := `
+		UPDATE users SET username = $1, email = $2, activated = $3
+		WHERE id = $4`
+
+	ctx, cancel := context.WithTimeout(ctx, queryTimeoutDuration)
+	defer cancel()
+
+	_, err := tx.ExecContext(ctx, query, user.Username, user.Email, user.Activated, user.ID)
+	return err
+}
+
+func (u *UsersRepository) deleteUserInvitations(ctx context.Context, userID int64, tx *sql.Tx) error {
+	query := `DELETE FROM user_invitations WHERE user_id = $1`
+
+	ctx, cancel := context.WithTimeout(ctx, queryTimeoutDuration)
+	defer cancel()
+
+	_, err := tx.ExecContext(ctx, query, userID)
+	return err
 }
